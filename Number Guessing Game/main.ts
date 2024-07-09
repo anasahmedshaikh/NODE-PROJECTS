d246ee3fcefd2866d4031ad15d3053a5e1e5e57d
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-09 12:48:42 +0500

    Update main.ts

--- Number Guessing Game/main.ts
+++ Number Guessing Game/main.ts
@@ -1,24 +1,31 @@
-#! usr/bin/env node
+#! /usr/bin/env node
+
 import inquirer from "inquirer";
-// code logic
-// generate a random number
-// ask user to guess the number
-// if user guess is correct, print "You won"
-// else print "You lost"
-const randomNumber = Math.floor(Math.random() * 10) + 1;
-console.log("Welcome to the guessing game");
-const answers: {
-    userGuess: number;
-} = await inquirer.prompt([
-    {
-        type: "number",
-        name: "userGuess",
-        message: "Enter your guess b/w 1 to 10: "
-    }
-]);
-if (answers.userGuess === randomNumber) {
-    console.log("proud moment You win");
-} else {
-    console.log("ohh you lost");
+function numGenerator (){Math.floor(Math.random() * 10) + 1;
+    return randomNumber;
 }
-console.log("the random number is:"+""+randomNumber);
+const randomNumber = numGenerator;
+async function start() {
+    console.log("Welcome to the guessing game");
+    const answers = await inquirer.prompt(
+      {
+        name: "userGuess",
+        type: "number",
+        message: "Enter your guess b/w 1 to 10: "
+      },
+    );
+  
+    const randomNumber = numGenerator();
+  
+    if (answers.userGuess === randomNumber) {
+      console.log("Proud moment! You win");
+    } else {
+      console.log("Ohh, you lost");
+    }
+  
+    console.log(`The random number is: ${randomNumber}`);
+  };
+  
+  // Call the main function
+  start();
+
